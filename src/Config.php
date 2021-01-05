a6ddcb489471605394d8cd77d6c15be6215694d7
Rafael Batista <rafael@devbatista.com>
2021-01-05 13:45:58 -0300

    teste

--- src/Config.php
+++ src/Config.php
@@ -1,28 +1,15 @@
 <?php
 namespace src;
 
-// class Config {
-//     const BASE_DIR = '';
-
-//     const DB_DRIVER = 'mysql';
-//     const DB_HOST = '162.241.60.235';
-//     const DB_DATABASE = 'devbat78_development';
-//     CONST DB_USER = 'devbat78_rafael';
-//     const DB_PASS = 'ShowdeBola#10';
-
-//     const ERROR_CONTROLLER = 'ErrorController';
-//     const DEFAULT_ACTION = 'index';
-// }
-
 class Config {
-    const BASE_DIR = '';
+	const BASE_DIR = '';
 
-    const DB_DRIVER = 'mysql';
-    const DB_HOST = 'localhost';
-    const DB_DATABASE = 'nqv';
-    CONST DB_USER = 'root';
-    const DB_PASS = '';
+	const DB_DRIVER = 'mysql';
+	const DB_HOST = '162.241.60.235';
+	const DB_DATABASE = 'devbat78_development';
+	CONST DB_USER = 'devbat78_rafael';
+	const DB_PASS = 'ShowdeBola#10';
 
-    const ERROR_CONTROLLER = 'ErrorController';
-    const DEFAULT_ACTION = 'index';
+	const ERROR_CONTROLLER = 'ErrorController';
+	 const DEFAULT_ACTION = 'index';
 }
