0ce3ba01d67301a74e19540f15d3f07da98f0e6e
Rafael Batista <rafael@geracaobr.com.br>
2021-03-24 18:45:37 -0300

    adicionando gitignore

--- src/Config.php
+++ src/Config.php
@@ -4,10 +4,11 @@ namespace src;
 class Config {
 	const BASE_DIR = '';
 	const DB_DRIVER = 'mysql';
-	const DB_HOST = '162.241.60.235';
-	const DB_DATABASE = 'devbat78_development';
-	CONST DB_USER = 'devbat78_rafael';
-	const DB_PASS = 'ShowdeBola#10';
+	const DB_HOST = 'localhost';
+	const DB_PORT = 3307;
+	const DB_DATABASE = 'nqv';
+	CONST DB_USER = 'root';
+	const DB_PASS = '';
 
 	const ERROR_CONTROLLER = 'ErrorController';
 	const DEFAULT_ACTION = 'index';
