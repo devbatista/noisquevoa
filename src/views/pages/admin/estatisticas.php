80bb35c5ec63cd184e539cd6f9663b2a0e1ded5b
Rafael Batista <rafael@devbatista.com>
2021-04-20 17:19:34 -0300

    diminuindo código para não mostrar na tela

--- src/views/pages/admin/estatisticas.php
+++ src/views/pages/admin/estatisticas.php
@@ -36,65 +36,7 @@
                                 <h5>Partidas</h5>
                             </div>
                             <div class="slidePartidas">
-                                <div>
-                                    <div class="ibox-content">
-                                        <div class="row text-center">
-                                            <div class="col-12">
-                                                <h5>Data e hora partida</h5>
-                                            </div>
-                                        </div>
-                                        <div class="row d-flex justify-content-center align-items-center">
-                                            <div class="col-4">
-                                                <img src="<?= $base ?>/assets/img/times/noisquevoa.png" alt="">
-                                            </div>
-                                            <div class="col-1">
-                                                2
-                                            </div>
-                                            <div class="col-1">
-                                                X
-                                            </div>
-                                            <div class="col-1">
-                                                1
-                                            </div>
-                                            <div class="col-4">
-                                                <img src="<?= $base ?>/assets/img/times/3.png" alt="">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="ibox-content">
-                                        <h2>Slide 2</h2>
-                                        <p>
-                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                            Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                            unknown printer took a galley of type and scrambled it to make a type specimen
-                                            book. It has survived not only five centuries, but also the leap.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="ibox-content">
-                                        <h2>Slide 3</h2>
-                                        <p>
-                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                            Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                            unknown printer took a galley of type and scrambled it to make a type specimen
-                                            book. It has survived not only five centuries, but also the leap.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="ibox-content">
-                                        <h2>Slide 4</h2>
-                                        <p>
-                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                            Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                            unknown printer took a galley of type and scrambled it to make a type specimen
-                                            book. It has survived not only five centuries, but also the leap.
-                                        </p>
-                                    </div>
-                                </div>
+                            
                             </div>
                             <!-- <div class="ibox-content" style="position: relative;height: 150px">
                             <canvas id="partidasChart"></canvas>
